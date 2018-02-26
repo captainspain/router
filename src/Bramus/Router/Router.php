fad173272f21bb5763c1fe82652068ca6a907f0e
captainspain <daniel@onlinesquare.nl>
2018-02-27 00:59:26 +0100

    Update Router.php
    modified protected getCurrentUri() to public, because it is easy.

--- src/Bramus/Router/Router.php
+++ src/Bramus/Router/Router.php
@@ -417,7 +417,7 @@ class Router
      *
      * @return string
      */
-    protected function getCurrentUri()
+    public function getCurrentUri()
     {
         // Get the current Request URI and remove rewrite base path from it (= allows one to run the router in a sub folder)
         $uri = substr($_SERVER['REQUEST_URI'], strlen($this->getBasePath()));
