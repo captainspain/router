3465ed7056c969c3b32d1c2cf3037e2254bab028
captainspain <daniel@onlinesquare.nl>
2018-02-27 03:09:38 +0100

    Update Router.php
    Adds the possibility to use arrays as patterns so you can add multiple patterns.

--- src/Bramus/Router/Router.php
+++ src/Bramus/Router/Router.php
@@ -51,20 +51,22 @@ class Router
     /**
      * Store a before middleware route and a handling function to be executed when accessed using one of the specified methods.
      *
-     * @param string          $methods Allowed methods, | delimited
-     * @param string          $pattern A route pattern such as /about/system
-     * @param object|callable $fn      The handling function to be executed
+     * @param string                $methods Allowed methods, | delimited
+     * @param string|array          $patterns A route pattern such as /about/system
+     * @param object|callable $fn   The handling function to be executed
      */
-    public function before($methods, $pattern, $fn)
+    public function before($methods, $patterns, $fn)
     {
-        $pattern = $this->baseRoute.'/'.trim($pattern, '/');
-        $pattern = $this->baseRoute ? rtrim($pattern, '/') : $pattern;
+        foreach ((array) $patterns as $pattern) {
+            $pattern = $this->baseRoute.'/'.trim($pattern, '/');
+            $pattern = $this->baseRoute ? rtrim($pattern, '/') : $pattern;
 
-        foreach (explode('|', $methods) as $method) {
-            $this->beforeRoutes[$method][] = array(
-                'pattern' => $pattern,
-                'fn' => $fn,
-            );
+            foreach (explode('|', $methods) as $method) {
+                $this->beforeRoutes[$method][] = array(
+                    'pattern' => $pattern,
+                    'fn' => $fn,
+                );
+            }
         }
     }
 
